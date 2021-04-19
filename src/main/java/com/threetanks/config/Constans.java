6f0c3369e4d6b6fff77be6a2797cfb31cf61ec61
HDUhxx <1255753087@qq.com>
2021-04-19 21:17:40 +0800

    默认

--- src/main/java/com/threetanks/config/Constans.java
+++ src/main/java/com/threetanks/config/Constans.java
@@ -12,7 +12,7 @@ public class Constans {
     public static final String clasName = "Solution";
     //部署的时候可能要更改，现在用类来实现，应该也可以用配置文件的形式实现，以后可以改成配置文件的形式
     //要考虑不同用户情况，可以选择在处理代码中加上uuid作为文件名，顺便把这个作为返回值返回给前台。
-    public static final String classPath = "C:\\compile\\";
-//    public static final String classPath = "/opt/compile/";
+//    public static final String classPath = "C:\\compile\\";
+    public static final String classPath = "/opt/compile/";
     public static final String excuteMainMethodName = "excuteMainMethodWithClass";
 }
